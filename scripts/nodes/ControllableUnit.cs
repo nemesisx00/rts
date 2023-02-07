257a5d04cd7e3aedd26f4b07498b6f3c60450e81
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-02-07 16:14:26 -0500

    Updated Unit Movement
    Added a Timer node to ControllableUnit; If the unit is struggling to get to its destination for too long, give up and stop moving.
    
    Added a TargetProximity property to give some leeway in reaching the destination.
    
    Removed Moving property, as it's not really necessary.

--- scripts/nodes/ControllableUnit.cs
+++ scripts/nodes/ControllableUnit.cs
@@ -8,34 +8,46 @@ namespace Rts.Nodes
 		private sealed class NodePaths
 		{
 			public const string SelectedOutline = "%SelectedOutline";
+			public const string Timer = "%Timer";
 		}
 		
 		[Export]
 		public string UnitName { get; set; }
 		[Export(PropertyHint.Range, "0,10,or_greater")]
 		public uint MoveSpeed { get; set; } = 2;
-		public bool Moving { get; private set; } = false;
+		[Export]
+		public uint TargetProximity { get; set; } = 1;
+		[Export]
+		public uint StruggleTime { get; set; } = 5;
 		
 		public bool Selected { get { return GetNode<Sprite2D>(NodePaths.SelectedOutline).Visible; } }
 		
 		private Vector2 targetPosition = Vector2.Zero;
 		private Sprite2D outline;
+		private Timer timer;
 		
-		public override void _Process(double delta)
+		public override void _PhysicsProcess(double delta)
 		{
-			if(Position.Equals(targetPosition))
-				Moving = false;
-			
-			if(Moving)
+			if(Position.DistanceTo(targetPosition) > TargetProximity)
 			{
 				Velocity = (targetPosition - Position) * MoveSpeed;
 				MoveAndSlide();
+				
+				if(timer.IsStopped() && GetSlideCollisionCount() > 0)
+				{
+					timer.Start();
+				}
 			}
 		}
 		
 		public override void _Ready()
 		{
+			targetPosition = Position;
 			outline = GetNode<Sprite2D>(NodePaths.SelectedOutline);
+			
+			timer = GetNode<Timer>(NodePaths.Timer);
+			timer.WaitTime = StruggleTime;
+			timer.Timeout += handleTimeout;
 		}
 		
 		public void deselect()
@@ -43,10 +55,15 @@ namespace Rts.Nodes
 			outline.Visible = false;
 		}
 		
+		private void handleTimeout()
+		{
+			if(GetSlideCollisionCount() > 0)
+				targetPosition = Position;
+		}
+		
 		public void move(Vector2 destination)
 		{
 			targetPosition = destination;
-			Moving = true;
 		}
 		
 		public void select()
