4a3b23d50613d42713c51e087d16d5ccf345ee78
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-02-07 13:56:44 -0500

    Implementing Unit Selection
    Implemented click-and-drag unit selection in Gameplay, using a physics collision query; Currently doesn't draw a box to visualize what will be selected yet.
    Implemented pressing Escape to deselect the selected units.
    Implemented pressing F10 to bring up the main menu scene; Stores the current Gameplay scene so it can be resumed instead of restarted.
    
    Added ControllableUnit node to represent a unit the player can select and, eventually, control. Selected status is based on the visibility of the SelectedOutline Sprite2D node; Currently using placeholder sprites for the main sprite and the outline sprite.
    
    Added a ControllableUnit instance to Gameplay scene for testing.

---
+++ nodes/ControllableUnit.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+namespace Rts.Nodes
+{
+	public partial class ControllableUnit : CharacterBody2D
+	{
+		private sealed class NodePaths
+		{
+			public const string SelectedOutline = "%SelectedOutline";
+		}
+		
+		[Export]
+		public string UnitName { get; set; }
+		
+		public bool Selected { get { return GetNode<Sprite2D>(NodePaths.SelectedOutline).Visible; } }
+		
+		private Sprite2D outline;
+		
+		public override void _Ready()
+		{
+			outline = GetNode<Sprite2D>(NodePaths.SelectedOutline);
+		}
+		
+		public void select()
+		{
+			outline.Visible = true;
+		}
+		
+		public void deselect()
+		{
+			outline.Visible = false;
+		}
+	}
+}
--- nodes/Gameplay.cs
+++ nodes/Gameplay.cs
@@ -1,9 +1,108 @@
 using Godot;
+using System.Collections.Generic;
+using Rts.Nodes.Autoload;
 
-public partial class Gameplay : Node2D
+namespace Rts.Nodes
 {
-	public override void _Ready()
+	public partial class Gameplay : Node2D
 	{
+		public bool Dragging { get; private set; } = false;
+		public Vector2 DragStart { get; private set; } = Vector2.Zero;
+		public List<ControllableUnit> SelectedUnits { get; set; } = new List<ControllableUnit>();
 		
+		public override void _Ready()
+		{
+			refreshSelectedUnits();
+		}
+		
+		public override void _UnhandledInput(InputEvent e)
+		{
+			if(e is InputEventKey iek)
+			{
+				processKeyboardPress(iek);
+			}
+			else if(e is InputEventMouseButton iemb)
+			{
+				switch(iemb.ButtonIndex)
+				{
+					case MouseButton.Left:
+						processLeftClick(iemb);
+						break;
+				}	
+			}
+		}
+		
+		public void refreshSelectedUnits()
+		{
+			SelectedUnits.Clear();
+			foreach(var node in GetChildren())
+			{
+				if(node is ControllableUnit unit && unit.Selected)
+					SelectedUnits.Add(unit);
+			}
+		}
+		
+		private void processKeyboardPress(InputEventKey iek)
+		{
+			switch(iek.Keycode)
+			{
+				case Key.Escape:
+					SelectedUnits.ForEach(u => u.deselect());
+					SelectedUnits.Clear();
+					break;
+				case Key.F10:
+					var sceneManager = GetNode<SceneManager>(SceneManager.NodePath);
+					sceneManager.storeCurrentScene();
+					sceneManager.changeScene(SceneManager.Scenes.MainMenu);
+					break;
+			}
+		}
+		
+		private void processLeftClick(InputEventMouseButton iemb)
+		{
+			if(iemb.Pressed)
+			{
+				Dragging = true;
+				DragStart = iemb.Position;
+			}
+			else if(Dragging)
+			{
+				Dragging = false;
+				var units = detectUnits(DragStart, iemb.Position);
+				if(units.Count > 0)
+				{
+					SelectedUnits.ForEach(u => u.deselect());
+					SelectedUnits.Clear();
+					SelectedUnits.AddRange(units);
+					SelectedUnits.ForEach(u => u.select());
+				}
+			}
+		}
+		
+		private List<ControllableUnit> detectUnits(Vector2 start, Vector2 end)
+		{
+			var shape = PhysicsServer2D.RectangleShapeCreate();
+			var size = new Vector2(end.x - start.x, end.y - start.y);
+			PhysicsServer2D.ShapeSetData(shape, size);
+			
+			var query = new PhysicsShapeQueryParameters2D();
+			query.ShapeRid = shape;
+			query.Transform = new Transform2D(0, (end + start) / 2);
+			var collisions = GetWorld2d().DirectSpaceState.IntersectShape(query);
+			
+			PhysicsServer2D.FreeRid(shape);
+			
+			var units = new List<ControllableUnit>();
+			foreach(var dict in collisions)
+			{
+				var collider = dict["collider"].As<ControllableUnit>();
+				if(collider is ControllableUnit unit && !units.Contains(unit))
+				{
+					units.Add(unit);
+				}
+			}
+			
+			return units;
+		}
 	}
 }
