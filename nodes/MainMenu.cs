390f57c4016321ff44857bcf6ec8a3a94e872726
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-02-07 13:59:18 -0500

    Updated MainMenu
    Added a Resume button to the MainMenu; It is only visible if the SceneManager currently has a stored scene.
    Updated logic so that Start Game will free any stored scene before changing to the Gameplay scene.
    Implemented Resume to restore the stored scene rather than changing to a new scene.

--- nodes/MainMenu.cs
+++ nodes/MainMenu.cs
@@ -1,20 +1,37 @@
 using Godot;
+using Rts.Nodes.Autoload;
 
-public partial class MainMenu : Control
+namespace Rts.Nodes
 {
-	public override void _Ready()
+	public partial class MainMenu : Control
 	{
-		GetNode<Button>("%StartGame").Pressed += startHandler;
-		GetNode<Button>("%Quit").Pressed += quitHandler;
-	}
-	
-	private void startHandler()
-	{
-		GetNode<SceneManager>(SceneManager.NodePath).changeScene(SceneManager.Scenes.Gameplay);
-	}
-	
-	private void quitHandler()
-	{
-		GetTree().Root.PropagateNotification((int)NotificationWmCloseRequest);
+		private SceneManager sceneManager;
+		public override void _Ready()
+		{
+			sceneManager = GetNode<SceneManager>(SceneManager.NodePath);
+			
+			var resumeButton = GetNode<Button>("%Resume");
+			resumeButton.Visible = sceneManager.HasStoredScene;
+			resumeButton.Pressed += resumeHandler;
+			
+			GetNode<Button>("%StartGame").Pressed += startHandler;
+			GetNode<Button>("%Quit").Pressed += quitHandler;
+		}
+		
+		private void resumeHandler()
+		{
+			sceneManager.restoreStoredScene();
+		}
+		
+		private void startHandler()
+		{
+			sceneManager.freeStoredScene();
+			sceneManager.changeScene(SceneManager.Scenes.Gameplay);
+		}
+		
+		private void quitHandler()
+		{
+			GetTree().Root.PropagateNotification((int)NotificationWmCloseRequest);
+		}
 	}
 }
